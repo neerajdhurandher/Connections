f453b3f020abd52ce9361cceaa13158a7f298f8a
neerajdhurandher01 <86510313+neerajdhurandher01@users.noreply.github.com>
2021-10-03 16:28:53 +0530

    Create Project

---
+++ src/components/signIn/signIn_page.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./signIn_page.css"
+
+function SignInPage() {
+
+    return (
+        <>
+        <div  className = "default_page">
+            <h1 className = "App-header">Connections</h1>
+            <div className = "signinButton">
+                <p>SigIn</p>
+
+            </div> 
+        </div>   
+        </>
+    );
+
+}
+
+export default SignInPage;
